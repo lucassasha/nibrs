86a240f0b382450decbcd20634cc5834d6dadb5f
Haiqi Wei <haiqi@search.org>
2022-09-26 11:52:02 -0500

    Add offenses for federal and tribal LEA reporting only.

--- tools/nibrs-common/src/main/java/org/search/nibrs/model/codes/OffenseCode.java
+++ tools/nibrs-common/src/main/java/org/search/nibrs/model/codes/OffenseCode.java
@@ -28,85 +28,109 @@ import java.util.regex.Pattern;
  */
 public enum OffenseCode {
 	
-	_720("720", "Animal Cruelty", "A"),
+	_720("720", "Animal Cruelty", "A", CrimeAgainstCode.Society),
 	/**
 	 * Arson
 	 */
-	_200("200", "Arson", "A"),
-	_13A("13A", "Aggravated Assault", "A"),
-	_13B("13B", "Simple Assault", "A"),
-	_13C("13C", "Intimidation", "A"),
-	_510("510", "Bribery", "A"),
-	_220("220", "Burglary/Breaking & Entering", "A"),
-	_250("250", "Counterfeiting/Forgery", "A"),
-	_290("290", "Destruction/Damage/Vandalism of Property", "A"),
-	_35A("35A", "Drug/Narcotic Violations", "A"),
-	_35B("35B", "Drug Equipment Violations", "A"),
-	_270("270", "Embezzlement", "A"),
-	_210("210", "Extortion/Blackmail", "A"),
-	_26A("26A", "False Pretenses/Swindle/Confidence Game", "A"),
-	_26B("26B", "Credit Card/Automated Teller Machine Fraud", "A"),
-	_26C("26C", "Impersonation", "A"),
-	_26D("26D", "Welfare Fraud", "A"),
-	_26E("26E", "Wire Fraud", "A"),
-	_26F("26F", "Identity Theft", "A"),
-	_26G("26G", "Fraud Offenses-Hacking/Computer Invasion", "A"),
-	_39A("39A", "Betting/Wagering", "A"),
-	_39B("39B", "Operating/Promoting/Assisting Gambling", "A"),
-	_39C("39C", "Gambling Equipment Violations", "A"),
-	_39D("39D", "Sports Tampering", "A"),
-	_09A("09A", "Murder & Non-negligent Manslaughter", "A"),
-	_09B("09B", "Negligent Manslaughter", "A"),
+	_200("200", "Arson", "A", CrimeAgainstCode.Property),
+	_13A("13A", "Aggravated Assault", "A", CrimeAgainstCode.Person),
+	_13B("13B", "Simple Assault", "A", CrimeAgainstCode.Person),
+	_13C("13C", "Intimidation", "A", CrimeAgainstCode.Person),
+	_510("510", "Bribery", "A", CrimeAgainstCode.Property),
+	_220("220", "Burglary/Breaking & Entering", "A", CrimeAgainstCode.Property),
+	_58A("58A", "Import Violations*", "A", CrimeAgainstCode.Society),
+	_58B("58B", "Export Violations*", "A", CrimeAgainstCode.Society),
+	_61A("61A", "Federal Liquor Offenses*", "A", CrimeAgainstCode.Society),
+	_61B("61B", "Federal Tobacco Offenses*", "A", CrimeAgainstCode.Society),
+	_620("620", "Wildlife Trafficking*", "A", CrimeAgainstCode.Society),
+	_250("250", "Counterfeiting/Forgery", "A", CrimeAgainstCode.Property),
+	_290("290", "Destruction/Damage/Vandalism of Property", "A", CrimeAgainstCode.Property),
+	_35A("35A", "Drug/Narcotic Violations", "A", CrimeAgainstCode.Society),
+	_35B("35B", "Drug Equipment Violations", "A", CrimeAgainstCode.Society),
+	_270("270", "Embezzlement", "A", CrimeAgainstCode.Property),
+	_103("103", "Espionage*", "A", CrimeAgainstCode.Society),
+	_210("210", "Extortion/Blackmail", "A", CrimeAgainstCode.Property),
+	_26A("26A", "False Pretenses/Swindle/Confidence Game", "A", CrimeAgainstCode.Property),
+	_26B("26B", "Credit Card/Automated Teller Machine Fraud", "A", CrimeAgainstCode.Property),
+	_26C("26C", "Impersonation", "A", CrimeAgainstCode.Property),
+	_26D("26D", "Welfare Fraud", "A", CrimeAgainstCode.Property),
+	_26E("26E", "Wire Fraud", "A", CrimeAgainstCode.Property),
+	_26F("26F", "Identity Theft", "A", CrimeAgainstCode.Property),
+	_26G("26G", "Fraud Offenses-Hacking/Computer Invasion", "A", CrimeAgainstCode.Property),
+	_26H("26H", "Money Laundering*", "A", CrimeAgainstCode.Property),
+	_39A("39A", "Betting/Wagering", "A", CrimeAgainstCode.Society),
+	_39B("39B", "Operating/Promoting/Assisting Gambling", "A", CrimeAgainstCode.Society),
+	_39C("39C", "Gambling Equipment Violations", "A", CrimeAgainstCode.Society),
+	_39D("39D", "Sports Tampering", "A", CrimeAgainstCode.Society),
+	_09A("09A", "Murder & Non-negligent Manslaughter", "A", CrimeAgainstCode.Person),
+	_09B("09B", "Negligent Manslaughter", "A", CrimeAgainstCode.Person),
 	/**
 	 * Justifiable Homicide
 	 */
-	_09C("09C", "Justifiable Homicide", "A"),
-	_64A("64A", "Human Trafficking, Commercial Sex Acts", "A"),
-	_64B("64B", "Human Trafficking, Involuntary Servitude", "A"),
-	_100("100", "Kidnapping/Abduction Person", "A"),
-	_23A("23A", "Pocket-picking", "A"),
-	_23B("23B", "Purse-snatching ", "A"),
-	_23C("23C", "Shoplifting ", "A"),
-	_23D("23D", "Theft From Building", "A"),
-	_23E("23E", "Theft From Coin-Operated Machine or Device ", "A"),
-	_23F("23F", "Theft From Motor Vehicle", "A"),
-	_23G("23G", "Theft of Motor Vehicle Parts or Accessories ", "A"),
-	_23H("23H", "All Other Larceny", "A"),
-	_240("240", "Motor Vehicle Theft", "A"),
-	_370("370", "Pornography/Obscene Material ", "A"),
-	_40A("40A", "Prostitution ", "A"),
-	_40B("40B", "Assisting or Promoting Prostitution ", "A"),
-	_40C("40C", "Purchasing Prostitution ", "A"),
-	_120("120", "Robbery ", "A"),
-	_11A("11A", "Rape ", "A"),
-	_11B("11B", "Sodomy ", "A"),
-	_11C("11C", "Sexual Assault With An Object", "A"),
-	_11D("11D", "Fondling ", "A"),
-	_36A("36A", "Incest ", "A"),
-	_36B("36B", "Statutory Rape ", "A"),
-	_280("280", "Stolen PropertySegment Offenses ", "A"),
-	_520("520", "Weapon Law Violations", "A"),
-	_90A("90A", "Bad Checks", "B"),
-	_90B("90B", "Curfew/Loitering/Vagrancy Violations", "B"),
-	_90C("90C", "Disorderly Conduct", "B"),
-	_90D("90D", "Driving Under the Influence", "B"),
-	_90E("90E", "Drunkenness", "B"),
-	_90F("90F", "Family Offenses, Nonviolent", "B"),
-	_90G("90G", "Liquor Law Violations", "B"),
-	_90H("90H", "Peeping Tom", "B"),
-	_90I("90I", "Runaway", "B"),
-	_90J("90J", "Trespass of Real Property", "B"),
-	_90Z("90Z", "All Other Offenses", "B")
+	_09C("09C", "Justifiable Homicide", "A", CrimeAgainstCode.Person),
+	_30A("30A", "Illegal Entry into the United States*", "A", CrimeAgainstCode.Society),
+	_30B("30B", "False Citizenship*", "A", CrimeAgainstCode.Society),
+	_30C("30C", "Smuggling Aliens*", "A", CrimeAgainstCode.Society),
+	_30D("30D", "Re-entry after Deportation*", "A", CrimeAgainstCode.Society),
+	_64A("64A", "Human Trafficking, Commercial Sex Acts", "A", CrimeAgainstCode.Person),
+	_64B("64B", "Human Trafficking, Involuntary Servitude", "A", CrimeAgainstCode.Person),
+	_100("100", "Kidnapping/Abduction Person", "A", CrimeAgainstCode.Person),
+	_49A("49A", "Harboring Escapee/ Concealing from Arrest*", "A", CrimeAgainstCode.Society),
+	_49B("49B", "Flight to Avoid Prosecution*", "A", CrimeAgainstCode.Society),
+	_49C("49C", "Flight to Avoid Deportation*", "A", CrimeAgainstCode.Society),
+	_23A("23A", "Pocket-picking", "A", CrimeAgainstCode.Property),
+	_23B("23B", "Purse-snatching ", "A", CrimeAgainstCode.Property),
+	_23C("23C", "Shoplifting ", "A", CrimeAgainstCode.Property),
+	_23D("23D", "Theft From Building", "A", CrimeAgainstCode.Property),
+	_23E("23E", "Theft From Coin-Operated Machine or Device ", "A", CrimeAgainstCode.Property),
+	_23F("23F", "Theft From Motor Vehicle", "A", CrimeAgainstCode.Property),
+	_23G("23G", "Theft of Motor Vehicle Parts or Accessories ", "A", CrimeAgainstCode.Property),
+	_23H("23H", "All Other Larceny", "A", CrimeAgainstCode.Property),
+	_240("240", "Motor Vehicle Theft", "A", CrimeAgainstCode.Property),
+	_370("370", "Pornography/Obscene Material ", "A", CrimeAgainstCode.Society),
+	_40A("40A", "Prostitution ", "A", CrimeAgainstCode.Society),
+	_40B("40B", "Assisting or Promoting Prostitution ", "A", CrimeAgainstCode.Society),
+	_40C("40C", "Purchasing Prostitution ", "A", CrimeAgainstCode.Society),
+	_120("120", "Robbery ", "A", CrimeAgainstCode.Property),
+	_11A("11A", "Rape ", "A", CrimeAgainstCode.Person),
+	_11B("11B", "Sodomy ", "A", CrimeAgainstCode.Person),
+	_11C("11C", "Sexual Assault With An Object", "A", CrimeAgainstCode.Person),
+	_11D("11D", "Fondling ", "A", CrimeAgainstCode.Person),
+	_36A("36A", "Incest ", "A", CrimeAgainstCode.Person),
+	_36B("36B", "Statutory Rape ", "A", CrimeAgainstCode.Person),
+	_280("280", "Stolen PropertySegment Offenses ", "A", CrimeAgainstCode.Property),
+	_101("101", "Treason*", "A", CrimeAgainstCode.Society),
+	_520("520", "Weapon Law Violations", "A", CrimeAgainstCode.Society),
+	_521("521", "Violation of National Firearm Act of 1934*", "A", CrimeAgainstCode.Society),
+	_522("522", "Weapons of Mass Destruction*", "A", CrimeAgainstCode.Society),
+	_526("526", "Weapons of Mass Destruction*", "A", CrimeAgainstCode.Society),
+	_90A("90A", "Bad Checks", "B", CrimeAgainstCode.Property),
+	_90B("90B", "Curfew/Loitering/Vagrancy Violations", "B", CrimeAgainstCode.Society),
+	_90C("90C", "Disorderly Conduct", "B", CrimeAgainstCode.Society),
+	_90D("90D", "Driving Under the Influence", "B", CrimeAgainstCode.Society),
+	_90E("90E", "Drunkenness", "B", CrimeAgainstCode.Society),
+	_90F("90F", "Family Offenses, Nonviolent", "B", CrimeAgainstCode.Society),
+	_90G("90G", "Liquor Law Violations", "B", CrimeAgainstCode.Society),
+	_90H("90H", "Peeping Tom", "B", CrimeAgainstCode.Society),
+	_90I("90I", "Runaway", "B", CrimeAgainstCode.Society), // No longer in the 2019 spec. 
+	_90J("90J", "Trespass of Real Property", "B", CrimeAgainstCode.Society),
+	_90K("90K", "Failure to Appear*", "B", CrimeAgainstCode.Society),
+	_90L("90L", "Federal Resource Violations*", "B", CrimeAgainstCode.Society),
+	_90M("90M", "Perjury*", "B", CrimeAgainstCode.Society),
+	_90Z("90Z", "All Other Offenses", "B", CrimeAgainstCode.PersonPropertyOrSociety)
 	;
 
 	public String code;
 	public String description;
 	public String group;
+	private CrimeAgainstCode crimeAgainst;
 	
-	private OffenseCode(String code, String description, String group) {
+	private OffenseCode(String code, String description, String group
+			, CrimeAgainstCode crimeAgainstCode) {
 		this.code = code;
 		this.description = description;
 		this.group = group;
+		this.crimeAgainst = crimeAgainstCode;
 	}
 	
 	public static final Set<OffenseCode> asSet() {
@@ -273,6 +297,14 @@ public enum OffenseCode {
 				_23F.code, _23G.code, _23H.code
 		).contains(code);
 
+	}
+
+	public CrimeAgainstCode getCrimeAgainst() {
+		return crimeAgainst;
+	}
+
+	public void setCrimeAgainst(CrimeAgainstCode crimeAgainst) {
+		this.crimeAgainst = crimeAgainst;
 	}
 
 }
